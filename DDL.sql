3adc63d831d278da9a2ee2920c7c47fc9ce94e0c
Martijn de Geus <martijnjgeus@gmail.com>
2020-03-12 21:44:41 +0100

    further normalisation

--- DDL.sql
+++ DDL.sql
@@ -1,13 +1,36 @@
 USE crime_us_2010;
 
-CREATE TABLE crimes
+DROP TABLE IF EXISTS crimeTypes;
+CREATE TABLE crimeTypes
 (
     CrimeCode            INT,
     CrimeCodeDescription VARCHAR(100)
 );
 
+DROP TABLE IF EXISTS premises;
 CREATE TABLE premises
 (
     PremiseCode        INT,
     PremiseDescription VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS areas;
+CREATE TABLE areas
+(
+    AreaId   INT,
+    AreaName VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS weapons;
+CREATE TABLE weapons
+(
+    WeaponCode        INT,
+    WeaponDescription VARCHAR(150)
+);
+
+DROP TABLE IF EXISTS statuses;
+CREATE TABLE statuses
+(
+    StatusCode        VARCHAR(3),
+    StatusDescription VARCHAR(100)
 );
--- DML.sql
+++ DML.sql
@@ -1,7 +1,7 @@
 USE crime_us_2010;
 
-TRUNCATE TABLE crimes;
-INSERT INTO crimes(CrimeCode, CrimeCodeDescription)
+TRUNCATE TABLE crimeTypes;
+INSERT INTO crimeTypes(CrimeCode, CrimeCodeDescription)
 SELECT CrimeCode, CrimeCodeDescription
 FROM import_table
 GROUP BY CrimeCode, CrimeCodeDescription
@@ -13,3 +13,33 @@ SELECT PremiseCode, PremiseDescription
 FROM import_table
 GROUP BY PremiseCode, PremiseDescription
 ORDER BY PremiseCode;
+
+TRUNCATE TABLE areas;
+INSERT INTO areas(AreaId, AreaName)
+SELECT AreaID, AreaName
+FROM import_table
+WHERE AreaID IS NOT NULL
+GROUP BY AreaID, AreaName
+ORDER BY AreaID;
+
+TRUNCATE weapons;
+INSERT INTO weapons(WeaponCode, WeaponDescription)
+SELECT WeaponUsedCode, WeaponDescription
+FROM import_table
+WHERE WeaponDescription IS NOT NULL
+GROUP BY WeaponUsedCode, WeaponDescription
+ORDER BY WeaponUsedCode;
+
+TRUNCATE statuses;
+INSERT INTO statuses(StatusCode, StatusDescription)
+SELECT StatusCode, StatusDescription
+FROM import_table
+WHERE StatusCode IS NOT NULL
+GROUP BY StatusCode, StatusDescription
+ORDER BY StatusCode;
+
+SELECT DRNumber, ROUND((LENGTH(i1.MOCodes) - LENGTH(REPLACE(i1.MOCodes, ' ', ''))) / LENGTH(' ')) + 1 AS count
+FROM import_table i1 JOIN import_table i2
+WHERE MOCodes <> '';
+
+# SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(MOCodes, ' ', 1), ' ', -1) AS MoCode FROM import_table WHERE DRNumber = 151520686;
